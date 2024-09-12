dd62bb55aa286a43880db07ea14bb8d9e3769e47
Nicholas Barlow <nicklowbar@gmail.com>
2024-09-12 13:43:28 -0700

    initial commit, add nengine, test fbuild definitions, remove googletest fork submodule

---
+++ src/nengine.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <src/utils/helpers.h>
+
+int main(int argc, char** argv)
+{
+    UNUSED(argc);
+    UNUSED(argv);
+
+    std::cout << "Hello World!" << std::endl;
+    return EXIT_SUCCESS;
+}
