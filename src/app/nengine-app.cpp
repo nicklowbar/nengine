88423586b6205bc24e3a6288f3e16c80f3137ff4
Nicholas Barlow <nicklowbar@gmail.com>
2024-09-24 02:45:57 -0700

    Refactor Vulkan extensions and layers required to use global variables, begin swap chain initialization, add queue family querying. Add logical device creation

--- src/app/nengine-app.cpp
+++ src/app/nengine-app.cpp
@@ -1,6 +1,10 @@
 #include "../core/include/nengine.h"
 #include "../utils/helpers.h"
 
+#ifdef __LINUX__
+#define VK_USE_PLATFORM_XLIB_XRANDR_EXT
+#endif
+
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_enum_string_helper.h> 
 #include <GLFW/glfw3.h>
@@ -9,13 +13,16 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 
+#include <bitset>
 #include <iostream>
-#include <sstream>
 #include <memory>
-#include <vector>
+#include <optional>
 #include <queue>
+#include <set>
+#include <sstream>
 #include <string>
 #include <string.h>
+#include <vector>
 
 const nengine_utils::version nengine_app_version = { 0, 0, 1, 0};
 const nengine_utils::version vulkan_api_version = { 0, 1, 3, 0};
@@ -69,6 +76,21 @@ void glfw_cleanup(GLFWwindow* window)
 }
 
 // Vulkan callbacks
+
+#ifndef NDEBUG
+// Initialize vulkan validation layers for error information.
+const std::vector<const char*> vulkan_required_validation_layers = {
+    "VK_LAYER_KHRONOS_validation",
+    "VK_LAYER_KHRONOS_profiles",
+};
+#endif
+
+// Required extensions for vulkan devices
+const std::vector<const char*> vulkan_required_device_extensions = {
+    "VK_KHR_portability_subset",
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+};
+
 VkResult vulkan_CreateDebugUtilsMessengerEXT(const VkInstance& instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
     auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
     if (func != nullptr) {
@@ -129,32 +151,26 @@ VkResult vulkan_initialize(VkInstance& vulkan_instance)
     vulkan_create_info.pApplicationInfo = &vulkan_app_info;
 
     // Make sure our Vulkan instance supports all required GLFW extensions
-    unsigned int glfw_extension_count;
-    std::vector<const char*> glfw_extensions;
+    unsigned int vulkan_enabled_extension_count;
+    std::vector<const char*> vulkan_enabled_extensions;
 
-    auto requiredInstanceExtensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count); 
-    for (unsigned int i = 0; i < static_cast<unsigned int>(glfw_extension_count); ++i)
+    auto requiredInstanceExtensions = glfwGetRequiredInstanceExtensions(&vulkan_enabled_extension_count); 
+    for (unsigned int i = 0; i < static_cast<unsigned int>(vulkan_enabled_extension_count); ++i)
     {
-        glfw_extensions.push_back(requiredInstanceExtensions[i]);
+        vulkan_enabled_extensions.push_back(requiredInstanceExtensions[i]);
     }
 
     #ifndef NDEBUG
-    glfw_extension_count++;
-    glfw_extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    vulkan_enabled_extension_count++;
+    vulkan_enabled_extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
     #endif // NDEBUG
     
-    vulkan_create_info.enabledExtensionCount = glfw_extension_count;
-    vulkan_create_info.ppEnabledExtensionNames = glfw_extensions.data();
+    vulkan_create_info.enabledExtensionCount = vulkan_enabled_extension_count;
+    vulkan_create_info.ppEnabledExtensionNames = vulkan_enabled_extensions.data();
 
     #ifndef NDEBUG
-    // Initialize vulkan validation layers for error information.
-    const std::vector<const char*> vulkan_validation_layers = {
-        "VK_LAYER_KHRONOS_validation",
-        "VK_LAYER_KHRONOS_profiles",
-    };
-
-    vulkan_create_info.enabledLayerCount = static_cast<unsigned int>(vulkan_validation_layers.size());
-    vulkan_create_info.ppEnabledLayerNames = vulkan_validation_layers.data();
+    vulkan_create_info.enabledLayerCount = static_cast<unsigned int>(vulkan_required_validation_layers.size());
+    vulkan_create_info.ppEnabledLayerNames = vulkan_required_validation_layers.data();
 
     // Validate our requested layers are available.
     {
@@ -171,7 +187,7 @@ VkResult vulkan_initialize(VkInstance& vulkan_instance)
         }
 
         // Check that our requested layers are available by this instance.
-        for (const char* layer_name : vulkan_validation_layers)
+        for (const char* layer_name : vulkan_required_validation_layers)
         {
             bool layer_found = false;
             for (const auto& layer_properties : vulkan_available_layers)
@@ -192,9 +208,9 @@ VkResult vulkan_initialize(VkInstance& vulkan_instance)
         }
 
         std::cout << "Using Vulkan validation layers: " <<  std::endl;
-        for (unsigned int i = 0; i < vulkan_validation_layers.size(); ++i)
+        for (unsigned int i = 0; i < vulkan_required_validation_layers.size(); ++i)
         {
-            std::cout << "\t" << vulkan_validation_layers[i] << std::endl;
+            std::cout << "\t" << vulkan_required_validation_layers[i] << std::endl;
         }
     }
     #endif // NDEBUG
@@ -214,18 +230,124 @@ VkResult vulkan_initialize(VkInstance& vulkan_instance)
     }
 
     std::cout << applicationName << ": Created Vulkan Instance with extensions: " <<  std::endl;
-    for (unsigned int i = 0; i < glfw_extension_count; ++i)
+    for (unsigned int i = 0; i < vulkan_enabled_extension_count; ++i)
     {
-        std::cout << "\t" << glfw_extensions[i] << std::endl;
+        std::cout << "\t" << vulkan_enabled_extensions[i] << std::endl;
     }
     return VK_SUCCESS;
 }
 
-bool vulkan_is_physical_device_suitable(VkPhysicalDevice physical_device)
+struct VulkanQueueFamilyIndices
+{
+    std::optional<uint32_t> graphics_family;
+    std::optional<uint32_t> present_family;
+
+    bool is_complete() {
+        return graphics_family.has_value()
+            && present_family.has_value();
+    }
+};
+
+struct VulkanSwapChainSupportDetails
+{
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+};
+
+void vulkan_print_queue_families(const VkPhysicalDevice& device)
+{
+    uint32_t queue_family_count;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, nullptr);
+    std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, queue_families.data());
+    VkPhysicalDeviceProperties device_properties;
+    vkGetPhysicalDeviceProperties(device, &device_properties);
+
+    int i = 0;
+    std::cout << applicationName << ": Enumerating Queue Families:" << std::endl;
+    for (auto queue_family : queue_families)
+    {
+        std::bitset<sizeof(uint32_t) * 4> queue_flag_bitset(queue_family.queueFlags);
+        std::cout   << "\tQueue Family : " << i << std::endl 
+                    << "\t\tNumber of Queues: " << queue_family.queueCount << std::endl
+                    << "\t\tFlags: " << queue_flag_bitset << std::endl;
+    }
+}
+
+bool vulkan_find_queue_families(const VkPhysicalDevice& device, VulkanQueueFamilyIndices& indices, const VkSurfaceKHR& surface)
+{
+    indices = {};
+
+    uint32_t queue_family_count;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, nullptr);
+    std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, queue_families.data());
+
+    uint32_t i = 0;
+    for (const auto& queue_family : queue_families)
+    {
+        if (queue_family.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            indices.graphics_family = i;
+        }
+
+        VkBool32 present_support = false;
+        vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &present_support);
+        if (present_support)
+        {
+            indices.present_family = i;
+        }
+
+        ++i;
+    }
+
+    if (!indices.is_complete())
+    {
+        std::ostringstream oss;
+        oss << applicationName << ": Vulkan :" << " device: " << device << " does not have complete queue families.";
+        throw std::runtime_error(oss.str());
+
+    }
+    return true;
+}
+
+bool vulkan_check_device_extension_support(VkPhysicalDevice physical_device)
+{
+    uint32_t extension_count;
+    vkEnumerateDeviceExtensionProperties(physical_device, nullptr, &extension_count, nullptr);
+
+    std::vector<VkExtensionProperties> available_extensions(extension_count);
+    vkEnumerateDeviceExtensionProperties(physical_device, nullptr, &extension_count, available_extensions.data());
+
+    std::set<std::string> required_device_extensions(vulkan_required_device_extensions.begin(), vulkan_required_device_extensions.end());
+    for (const auto& extension : available_extensions)
+    {
+        required_device_extensions.erase(extension.extensionName);
+    }
+
+    return required_device_extensions.empty();
+}
+
+
+bool vulkan_is_physical_device_suitable(const VkPhysicalDevice& physical_device,
+                                        const VkPhysicalDeviceProperties& device_properties,
+                                        const VkPhysicalDeviceFeatures& device_features,
+                                        const VkPhysicalDeviceMemoryProperties& device_memory_properties,
+                                        const VkSurfaceKHR& surface)
 {
     // TODO: consider cases where we may want to disregard a potential device.
     UNUSED(physical_device);
-    return true;
+    UNUSED(device_properties);
+    UNUSED(device_memory_properties);
+    UNUSED(device_features);
+    
+    bool is_suitable = true;
+    VulkanQueueFamilyIndices device_queue_family_indices;
+    vulkan_find_queue_families(physical_device, device_queue_family_indices, surface);
+    is_suitable == device_queue_family_indices.is_complete()
+                && vulkan_check_device_extension_support(physical_device);
+    return is_suitable;
 }
 
 unsigned long long vulkan_get_physical_device_score(const VkPhysicalDevice& physical_device,
@@ -250,7 +372,7 @@ unsigned long long vulkan_get_physical_device_score(const VkPhysicalDevice& phys
             break;
         case VK_PHYSICAL_DEVICE_TYPE_OTHER:
         default:
-            score += 0;
+            score += 0; 
             break;
     }
 
@@ -264,7 +386,7 @@ unsigned long long vulkan_get_physical_device_score(const VkPhysicalDevice& phys
     return score;
 }
 
-VkResult vulkan_pick_physical_device(const VkInstance& vulkan_instance, VkPhysicalDevice& vulkan_physical_device)
+VkResult vulkan_pick_physical_device(const VkInstance& vulkan_instance, VkPhysicalDevice& vulkan_physical_device, const VkSurfaceKHR& surface)
 {
     unsigned int vulkan_physical_device_count = 0;
     vkEnumeratePhysicalDevices(vulkan_instance, &vulkan_physical_device_count, nullptr);
@@ -295,13 +417,13 @@ VkResult vulkan_pick_physical_device(const VkInstance& vulkan_instance, VkPhysic
 
         std::cout << "\t" << device_properties.deviceName << ": ";
 
-        if (vulkan_is_physical_device_suitable(device))
+        if (vulkan_is_physical_device_suitable(device, device_properties, device_features, device_memory_properties, surface))
         {
             unsigned int device_score = vulkan_get_physical_device_score(device, device_properties, device_features, device_memory_properties);
             suitable_vulkan_devices.push(std::make_tuple(device, device_score));
             std::cout << " - Capability Score: " << device_score;
         }
-        else 
+        else
         {
             std::cout << " - NOT SUITABLE";
         }
@@ -328,7 +450,7 @@ VkResult vulkan_pick_physical_device(const VkInstance& vulkan_instance, VkPhysic
 VkResult vulkan_initialize_debug_utils(const VkInstance& vulkan_instance, VkDebugUtilsMessengerEXT& vulkan_debug_messenger)
 {
     // enable debug messaging from the Vulkan instance
-    VkDebugUtilsMessengerCreateInfoEXT vulkan_debug_utils_messenger_createInfo{};
+    VkDebugUtilsMessengerCreateInfoEXT vulkan_debug_utils_messenger_createInfo = {};
     vulkan_debug_utils_messenger_createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
     vulkan_debug_utils_messenger_createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT 
                                                             | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
@@ -351,8 +473,99 @@ VkResult vulkan_initialize_debug_utils(const VkInstance& vulkan_instance, VkDebu
     return result;
 }
 
-void vulkan_cleanup(VkInstance& vulkan_instance, VkDebugUtilsMessengerEXT& vulkan_debug_messenger)
+void vulkan_create_logical_device(VkPhysicalDevice& physical_device, VkDevice& device, VkQueue& graphics_queue, VkSurfaceKHR& surface)
 {
+    std::cout << applicationName << ": Creating Vulkan logical device." << std::endl;
+    
+    VkPhysicalDeviceProperties physical_device_properties = {};
+    vkGetPhysicalDeviceProperties(physical_device, &physical_device_properties);
+    
+    std::vector<VkExtensionProperties> physical_device_extension_properties;
+    
+    
+    std::cout << applicationName << ": Physical Device supports extensions:" << std::endl;
+    for(auto& extension : physical_device_extension_properties)
+    {
+        std::cout << "\t" << extension.extensionName << std::endl;
+    }
+
+    std::vector<const char*> device_extensions(vulkan_required_device_extensions); 
+
+    VulkanQueueFamilyIndices queue_family_indices;
+    vulkan_find_queue_families(physical_device, queue_family_indices, surface);
+    vulkan_print_queue_families(physical_device);
+
+    std::cout << "\t" << "Using Graphics Queue: " << queue_family_indices.graphics_family.value() << std::endl;
+
+    VkDeviceQueueCreateInfo queue_create_info = {};
+    queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queue_create_info.queueFamilyIndex = queue_family_indices.graphics_family.value();
+    queue_create_info.queueCount = 1;
+
+    float queue_priority = 1.0f;
+    queue_create_info.pQueuePriorities = &queue_priority;
+
+    VkPhysicalDeviceFeatures physical_device_features = {};
+
+    VkDeviceCreateInfo logical_device_create_info = {};
+    logical_device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    logical_device_create_info.pQueueCreateInfos = &queue_create_info;
+    logical_device_create_info.queueCreateInfoCount = 1;
+    logical_device_create_info.pEnabledFeatures = &physical_device_features;
+    logical_device_create_info.enabledExtensionCount = device_extensions.size();
+    logical_device_create_info.ppEnabledExtensionNames = device_extensions.data();
+    logical_device_create_info.enabledLayerCount = 0;
+    #ifndef NDEBUG
+    logical_device_create_info.enabledLayerCount = static_cast<uint32_t>(vulkan_required_validation_layers.size());
+    logical_device_create_info.ppEnabledLayerNames = vulkan_required_validation_layers.data();
+    #endif
+
+    if (vkCreateDevice(physical_device, &logical_device_create_info, nullptr, &device) != VK_SUCCESS)
+    {
+        std::ostringstream oss;
+        oss << applicationName << ": Vulkan - Failed to create logical device.";
+        throw std::runtime_error(oss.str());
+    }
+
+    std::cout << applicationName << ": Created Vulkan device with extensions:" << std::endl;
+    
+    for(auto& extensionName : device_extensions)
+    {
+        std::cout << "\t" << extensionName << std::endl;
+    }
+
+    vkGetDeviceQueue(device, queue_family_indices.graphics_family.value(), 0, &graphics_queue);
+}
+
+VulkanSwapChainSupportDetails vulkan_query_swap_chain_support_details(VkPhysicalDevice& physical_device, VkSurfaceKHR& surface)
+{
+    VulkanSwapChainSupportDetails details;
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physical_device, surface, &details.capabilities);
+
+    uint32_t format_count;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physical_device, surface, &format_count, nullptr);
+    if (format_count != 0)
+    {
+        details.formats.resize(format_count);
+        vkGetPhysicalDeviceSurfaceFormatsKHR(physical_device, surface, &format_count, details.formats.data());
+    }
+
+
+
+    //uint32_t present_mode_count;
+    //vkGetPhysicalDeviceSurfacePresentModesKHR(physical_device, surface, &present_mode_count, nullptr);
+    //if (present_mode_count != 0)
+    //{
+        
+    //}
+
+    return details;
+}
+
+void vulkan_cleanup(VkInstance& vulkan_instance, VkDebugUtilsMessengerEXT& vulkan_debug_messenger, VkDevice& vulkan_device, VkSurfaceKHR& vulkan_surface)
+{
+    vkDestroySurfaceKHR(vulkan_instance, vulkan_surface, nullptr);
+    vkDestroyDevice(vulkan_device, nullptr);
     vulkan_DestroyDebugUtilsMessengerEXT(vulkan_instance, vulkan_debug_messenger, nullptr);
     vkDestroyInstance(vulkan_instance, nullptr);
 }
@@ -365,23 +578,48 @@ int main(int argc, char** argv)
     nengine_config config;
 
     glfw_initialize();
-    GLFWwindow* window = glfw_create_window(config);
 
     VkInstance vulkan_instance = VK_NULL_HANDLE;
     vulkan_initialize(vulkan_instance);
 
-    VkDebugUtilsMessengerEXT vulkan_debug_messenger = VK_NULL_HANDLE; 
+    VkDebugUtilsMessengerEXT vulkan_debug_messenger = VK_NULL_HANDLE;
     #ifndef NDEBUG
     vulkan_initialize_debug_utils(vulkan_instance, vulkan_debug_messenger);
     #endif // NDEBUG
 
+    VkSurfaceKHR surface = VK_NULL_HANDLE;
+
+    // Finally, create the window and rendering surface with GLFW.
+    GLFWwindow* window = glfw_create_window(config);
+
+    if (glfwCreateWindowSurface(vulkan_instance, window, nullptr, &surface) != VK_SUCCESS)
+    {
+        std::ostringstream oss;
+        oss << applicationName << ": failed to create window surface." << std::endl;
+        throw std::runtime_error(oss.str());
+    }
+
     // pick a physical device
-    VkPhysicalDevice vulkan_physical_device = VK_NULL_HANDLE; 
-    vulkan_pick_physical_device(vulkan_instance, vulkan_physical_device);
-    UNUSED(vulkan_physical_device);
+    VkPhysicalDevice vulkan_physical_device = VK_NULL_HANDLE;
+    vulkan_pick_physical_device(vulkan_instance, vulkan_physical_device, surface);
+
+    std::cout << applicationName << ": Created window surface.";
+
+    // create vulkan device
+    VkDevice vulkan_device = VK_NULL_HANDLE;
+    VkQueue vulkan_graphics_queue = VK_NULL_HANDLE;
+    vulkan_create_logical_device(vulkan_physical_device, vulkan_device, vulkan_graphics_queue, surface);
+
+    // initialize the swapchain
 
     // More application initialization
     auto engine_instance = std::make_unique<nengine>();
+    std::cout << applicationName    << ": Initialized NeNgine v"
+                                    << nengine::nengine_version.variant << "."
+                                    << nengine::nengine_version.major << "."
+                                    << nengine::nengine_version.minor << "."
+                                    << nengine::nengine_version.variant
+                                    << std::endl;
 
     // Application main loop, pump OS events, calling handler callbacks via GLFW.
     engine_instance->initialize();
@@ -390,8 +628,10 @@ int main(int argc, char** argv)
         glfwPollEvents();
     }
 
+    engine_instance->shutdown();
+
     // cleanup Vulkan instance and dependencies
-    vulkan_cleanup(vulkan_instance, vulkan_debug_messenger);
+    vulkan_cleanup(vulkan_instance, vulkan_debug_messenger, vulkan_device, surface);
 
     // cleanup GLFW window and instance
     glfw_cleanup(window);
