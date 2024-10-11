473cea91e725d46fea04416ace0149a5421c0e8a
Nicholas Barlow <nicklowbar@gmail.com>
2024-10-11 00:27:09 -0700

    add shaderc dependency, fix glm build definition, update debian dependencies

--- doc/setup_debian.sh
+++ doc/setup_debian.sh
@@ -3,3 +3,8 @@ wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc
 sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
 
 #install dependencies
+# Read each line from the input file and install the package
+input_file="./dependencies_debian.txt"
+while IFS= read -r package; do
+    sudo apt-get install -y "$package"
+done < "$input_file"
---
+++ src/core/include/nengine-shadercompiler.h
@@ -0,0 +1,11 @@
+#prama once
+
+#include "../../utils/helpers.h"
+#include <string>
+
+struct shader_compiler_config
+{
+    const std::string vertex_shader_path = nullptr;
+    const std::string fragment_shader_path = nullptr;
+    const std::string output_path = nullptr;
+};
